14da05f0b682d3aa2ce05e4b5ca860325e13f937
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-19 18:35:07 +0700

    Changed classname

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,19 +1,20 @@
 import Image from "next/image";
 import type { Product } from "@/types/products";
-import "../styles/productCard.css";
+import "../styles/product_card.css";
 import Link from "next/link";
 
 export function ProductCard({ product }: { product: Product }) {
     return (
-        <div key={product.prod_id} className='product'>
-            <Link href={`/product?prod_id=${product.prod_id}`}>
+        <div key={product.prod_id} className="product-card">
+            <Link href={`/detail?prod_id=${product.prod_id}`}>
                 <Image src={"/products/" + product.prod_id + ".jpg"}
                     alt={product.prod_name}
                     width={300}
                     height={300}
+                    className="product-img"
                 />
-                <p className="prod-name">{product.prod_name}</p>
-                <p className="prod-price">{product.price} บาท</p>
+                <p className="product-card-name">{product.prod_name}</p>
+                <p className="product-card-price">{product.price} บาท</p>
             </Link>
         </div>
     )
@@ -21,7 +22,7 @@ export function ProductCard({ product }: { product: Product }) {
 
 export function ProductCards({ products }: { products: Product[] }) {
     return (
-        <div className="products">
+        <div className="product-cards">
             {
                 products.map((item : Product) =>  (
                     <ProductCard product={item} key={item.prod_id} />
