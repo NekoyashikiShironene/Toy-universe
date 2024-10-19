3375335916ed5c92967035df1d4617983cb49a57
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-19 14:35:05 +0700

    Initial cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,16 +3,15 @@ import "../../styles/cart.css";
 import connectToDatabase from '@/utils/db';
 import Image from 'next/image';
 import { Product } from '../../types/products';
-import { CartTable } from '@/components/CartTable';
+import { CartItems } from '@/components/CartItems';
+import { ContentContainer } from '@/components/Containers';
  
 //SELECT product.prod_name, product.price , cart.quantity FROM `cart` JOIN product ON cart.prod_id = product.prod_id WHERE cart.cus_id = 1;
 
 const testDatabaseConnection = async () => {
   try {
     const connection = await connectToDatabase();
-    console.log('Database connection successful!');
     const [results, fields] = await connection.query("SELECT product.prod_id,product.prod_name, product.price , cart.quantity FROM `cart` JOIN product ON cart.prod_id = product.prod_id WHERE cart.cus_id = 1");
-    console.log(results, fields);
     await connection.end();
     
     return results;
@@ -34,8 +33,9 @@ export default async function page() {
 
 
   return (
-    <>
-      <CartTable products={result} />
-    </>
+    <ContentContainer>
+      <h1>Shopping Cart</h1>
+      <CartItems products={result}/>
+    </ContentContainer>
   )
 }
