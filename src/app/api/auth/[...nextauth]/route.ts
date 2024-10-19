e18eb7fb3547ebd72be5ee4c4b289cb59a6a92d7
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-19 14:34:34 +0700

    Initial authentication route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,8 +3,9 @@ import type { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google"
 
-type Req = Pick<RequestInternal, "body" | "headers" | "query" | "method">;
+// type Req = Pick<RequestInternal, "body" | "headers" | "query" | "method">;
 type Credentials = Record<"username" | "password", string> | undefined;
+
 
 export const authOptions: AuthOptions = {
     secret: "asjkjaofjasilrjeoijfaopufw3fui90urt90439opfvajf",
@@ -19,7 +20,7 @@ export const authOptions: AuthOptions = {
                 username: { label: "Username", type: "text", placeholder: "Baramee123" },
                 password: { label: "Password", type: "password" }
             },
-            authorize: function (credentials: Credentials, req: Req): Awaitable<User | null> {
+            authorize: function (credentials: Credentials): Awaitable<User | null> {
                 if (!credentials)
                     return null;
 
@@ -27,19 +28,74 @@ export const authOptions: AuthOptions = {
                 // credentials?.password;
                 // Add check username & password logic here
 
-                throw new Error("Function not implemented.");
+                // check username & encrypted password in database
+                // Get email, image path
+
+                const { username, password } = credentials;
+
+                if (username === "admin" && password === "admin") {
+                    const user = { 
+                        id: "12433452", 
+                        name: "Admin User", 
+                        email: "Bla@gmail.com", 
+                        image: "/user/.png",
+                    };
+
+                    return user;
+                }
+
+                return null;
             }
         })
     ],
     callbacks: {
-        async signIn({ account, profile }) {
-            console.log(account);
-            if (account?.provider === "google") {
-                console.log(profile);
+        async session({ session, token }) {
+            return { ...session, user: { ...session.user, id: token.id, role: token.role, cart: token.cart } };
+        }, 
+        
+        async jwt({ token, trigger, session, user, account, profile }) {
+            if (trigger === "update" && session?.name) {
+                token.name = session.name
+            }
 
+            if (trigger === "update" && session?.cart) {
+                    token.cart = session.cart;
+              }
+
+            if (account?.provider === "google") {
+                // get user id from email
+                const { email } = profile;
+                token.id = "1212312121";
+                token.role = "google_admin";
+                token.cart = []
+                //
+            } 
+            // Credential
+            else if (user) {
+                token.id = user.id
+                token.role = "credentials_admin";
+                token.cart = []
+            }
+
+            console.log("token", token);
+            console.log("account", account);
+            console.log("profile", profile);
+              
+            return token
+          },
+          
+        async signIn({ account }) {
+            
+            if (account?.provider === "google") {
+                return true;
+            } 
+
+            else if (account?.provider === "credentials") {
                 // Add logic
                 return true;
             }
+
+            return false;
         }
     }
 };
