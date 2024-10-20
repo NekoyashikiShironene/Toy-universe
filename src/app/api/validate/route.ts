8a0097b58a02d09d126b8949be6c3d0b751ae806
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-20 17:41:32 +0700

    Fixed type error with QueryResult

--- src/app/api/validate/route.ts
+++ src/app/api/validate/route.ts
@@ -1,26 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectToDatabase from "@/utils/db";
 
+import type { ICustomer } from "@/types/db";
 
 export async function GET(req: NextRequest) {
     const username = req.nextUrl.searchParams.get('username');
     const email = req.nextUrl.searchParams.get('email');
-    
+
     const connection = await connectToDatabase();
 
-    let results: any[] = [], fields: any;
-    if (username) {
-        [results, fields] = await connection.query("SELECT username FROM customer WHERE username = ?", [username]);
-        
-    }
-    else if (email){
-        [results, fields] = await connection.query("SELECT email FROM customer WHERE email = ?", [email]);
-        
-    }
-    
+    let results: ICustomer[] = [];
+    if (username)
+        [results] = await connection.query<ICustomer[]>("SELECT username FROM customer WHERE username = ?", [username]);
+
+    else if (email)
+        [results] = await connection.query<ICustomer[]>("SELECT email FROM customer WHERE email = ?", [email]);
+
+
+
     await connection.end();
 
     const valid = (results.length !== 0);
-    
-    return NextResponse.json({valid});
+
+    return NextResponse.json({ valid });
 }
