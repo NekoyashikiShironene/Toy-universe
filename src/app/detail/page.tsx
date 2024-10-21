7292bd03e91934766ce2ab147d92ed7f2a1f7ea6
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-21 18:57:06 +0700

    Added quantity increment

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,57 +1,88 @@
-import React from 'react';
+"use client";
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-import { Product } from '../../types/products';
-import connectToDatabase from '@/utils/db';
+import type { Product } from '../../types/products';
 import "../../styles/detail.css";
 import Link from 'next/link';
-
-const getProductDetails = async (prod_id: string) => {
-  try {
-    const connection = await connectToDatabase();
-    const [results] = await connection.query(
-      "SELECT * FROM product WHERE prod_id = ?",
-      [prod_id]
-    );
-    await connection.end();
-
-    return results[0];
-  } catch (error) {
-    console.error('Database connection failed:', error);
-  }
-};
+import { useSession } from "next-auth/react";
+import { UserSession } from '@/types/session';
 
 type Prop = {
   searchParams: { [key: string]: string | string[] | undefined }
 }
 
-// /product?prod_id=1002
-// /product/1002
-export default async function ProductDetailPage({ searchParams }: Prop) {
+export default function ProductDetailPage({ searchParams }: Prop) {
+  const { data: session, update } = useSession();
+
   const { prod_id } = searchParams; // ดึง prod_id จาก query params
+  const [ product, setProduct ] = useState<Product | undefined>();
+  const [ quantity, setQuantity ] = useState<number>(1);
+  
+  useEffect(() => {
+    const getProductDetails = async (prod_id: string) => {
+      try {
+        const res = await fetch("api/product?prod_id=" + prod_id);
+        const data = (await res.json()).data;
+        setProduct(data[0]);
+        
+      } catch (error) {
+        console.error('Database connection failed:', error);
+      }
+    };
+    getProductDetails(prod_id as string);
+  }, [prod_id]);
 
+  const handlePlus = () => {
+    if (!product){
+      return;
+    }
 
-  const product: Product = await getProductDetails(prod_id as string);
+    if (quantity < product?.quantity){
+      setQuantity(quantity + 1);
+    }
+  }
+
+  const handleMinus = () => {
+    if (quantity > 1){
+      setQuantity(quantity - 1);
+    }
+  }
+
+  const addProductToCart = () => {
+      const newProduct = {
+        prod_id: prod_id,
+        quantity: quantity
+      };
+      const user = session?.user as UserSession;
+      update({cart: [...(user?.cart), newProduct]});
+      
+  }
 
   return (
     <div className="product-detail">
       <div className="image-container">
         <Image
-          src={`/products/${product.prod_id}.jpg`}
-          alt={product.prod_name}
+          src={`/products/${prod_id}.jpg`}
+          alt={product?.prod_name ?? ""}
           width={300}
           height={300}
           className="product-image"
         />
       </div>
       <div className="details">
-        <h1 className="product-name">{product.prod_name}</h1>
-        <p className="product-price">ราคา: <span className="price">{product.price} บาท</span></p>
-        <p className="product-description">รายละเอียดสินค้า: <br />{product.description}</p>
-        <p className="stock">จำนวนสินค้าในคลัง: {product.quantity} ชิ้น</p>
-        <button className="add-to-cart">เพิ่มลงในตะกร้า</button>
-        <Link href={"/payment?prod_id=" + product.prod_id}>
+        <h1 className="product-name">{product?.prod_name}</h1>
+        <p className="product-price">ราคา: <span className="price">{product?.price} บาท</span></p>
+        <p className="product-description">รายละเอียดสินค้า: <br />{product?.description}</p>
+        <p className="stock">จำนวนสินค้าในคลัง: {product?.quantity} ชิ้น</p>
+        <div className="item-quantity">
+          <button onClick={handleMinus}>-</button>
+          <input type="number" value={quantity} min={1} max={product?.quantity} readOnly/>
+          <button onClick={handlePlus}>+</button>
+        </div>
+        <button className="add-to-cart" onClick={addProductToCart}>เพิ่มลงในตะกร้า</button>
+        <Link href={"/payment?prod_id=" + product?.prod_id}>
           <button type="submit" className="buy">
-              สั่งซื้อ
+            สั่งซื้อ
           </button>
         </Link>
       </div>
