103953054c1d78ab5ccdf37643921762318acc58
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-21 18:57:43 +0700

    Removed field variable

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import "../../styles/cart.css";
 import connectToDatabase from '@/utils/db';
-import Image from 'next/image';
 import { Product } from '../../types/products';
 import { CartItems } from '@/components/CartItems';
 import { ContentContainer } from '@/components/Containers';
- 
+
 //SELECT product.prod_name, product.price , cart.quantity FROM `cart` JOIN product ON cart.prod_id = product.prod_id WHERE cart.cus_id = 1;
 
 const testDatabaseConnection = async () => {
   try {
     const connection = await connectToDatabase();
-    const [results, fields] = await connection.query("SELECT product.prod_id,product.prod_name, product.price , cart.quantity FROM `cart` JOIN product ON cart.prod_id = product.prod_id WHERE cart.cus_id = 1");
+    const [results] = await connection.query("SELECT product.prod_id,product.prod_name, product.price , cart.quantity FROM `cart` JOIN product ON cart.prod_id = product.prod_id WHERE cart.cus_id = 200001");
     await connection.end();
-    
+
     return results;
 
 
@@ -24,18 +23,25 @@ const testDatabaseConnection = async () => {
 
 
 export default async function page() {
-
   const result = await testDatabaseConnection() as Product[];
-
-  // const totalPrice: number = result.reduce((acc: number, item: any) => {
-  //   return acc + item.price * item.quantity;
-  // }, 0);
 
 
   return (
     <ContentContainer>
       <h1>Shopping Cart</h1>
-      <CartItems products={result}/>
+      <div className="cart-page">
+        <CartItems products={result} />
+
+        <div className="cart-item-summary">
+          <h3 className="text-summary">{`3 items (1200 ฿)`}</h3>
+          <p className="text-summary">Shipping, taxes, and discounts will be calculated at checkout</p>
+          <div className="btn-container">
+            <button className="continue-shopping-btn">Continue Shopping</button>
+            <button className="checkout-btn">Checkout</button>
+          </div>
+
+        </div>
+      </div>
     </ContentContainer>
   )
 }
