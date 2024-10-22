3fefc218e6bb78765a76d7d340585ef51d16b6a4
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-22 16:20:03 +0700

    Completed register form validation

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -8,130 +8,163 @@ import '../../styles/register.css';
 import Image from 'next/image';
 import { register } from '@/actions/register';
 
+type InputError = {
+    value: boolean,
+    message: string 
+}
+
 export default function Register() {
     const { data: session } = useSession();
     const router = useRouter();
 
     console.log(session);
-    if (session) 
+    if (session)
         router.push("/");
 
-    const [ username, setUsername ] = useState<string>('');
-    const [ email, setEmail ] = useState<string>('');
-    const [ userError, setUserError ] = useState<boolean>(false);
-    const [ emailError, setEmailError ] = useState<boolean>(false);
+    const [username, setUsername] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [userError, setUserError] = useState<InputError>({value: false, message: ""});
+    const [emailError, setEmailError] = useState<InputError>({value: false, message: ""});
+    const [passwordError, setPasswordError] = useState<InputError>({value: false, message: ""});
 
     const [state, formAction] = useFormState(register, null);
 
-    const validateUsername = async (username: string) => {
-        if (username) {
-            const res = await fetch('api/validate?username=' + username);
-            const valid = (await res.json()).valid;
-
-            const username_input = document.getElementById("username") as HTMLInputElement;
-
-            if (!username_input)
-                return;
-
-            if (valid && username_input.value) {
-                username_input.style.borderColor = 'rgb(247, 60, 60)'; // red
-                setUserError(true);
-            }
-            else {
-                username_input.style.borderColor = 'rgb(14, 204, 14)'; // green
-                setUserError(false);
-            }
-        }
-    }
-
-    const validateEmail = async (email: string) => {
-        if (email) {
-            const res = await fetch('api/validate?email=' + email);
-            const valid = (await res.json()).valid;
-
-            const email_input = document.getElementById("email") as HTMLInputElement;
-
-            if (!email_input)
-                return;
-
-            if (valid && email_input.value) {
-                email_input.style.borderColor = 'rgb(247, 60, 60)'; 
-                setEmailError(true);
-            }
-            else {
-                email_input.style.borderColor = 'rgb(14, 204, 14)';
-                setEmailError(false);
-            }
-        }
-    }
 
     useEffect(() => {
-        if (!username)
-            return;
-        async function checkInput(){
-            await validateUsername(username);
-        }
+        const validateUsername = async () => {
+            const username_input = document.getElementById("username") as HTMLInputElement;
+            const username = username_input.value;
 
-        checkInput();
+            if (username_input.value) {
+                const res = await fetch('api/validate?username=' + username);
+                const valid = (await res.json()).valid;
+                
+                //check pattern
+                if (username_input.validity.patternMismatch) {
+                    setUserError({value: true, message: 'Pattern incorrect'});
+                    username_input.style.borderColor = 'rgb(247, 60, 60)';                  
+                }
+                //check if exist or not
+                else if (valid && username_input.value) {
+                    username_input.style.borderColor = 'rgb(247, 60, 60)'; // red
+                    setUserError({value: true, message: 'Username already exists'});
+                }
+                else {
+                    username_input.style.borderColor = 'rgb(14, 204, 14)'; // green
+                    setUserError({value: false, message: ''});
+                }
+            }
+            else {
+                username_input.style.borderColor = 'black';
+                setUserError({value: false, message: ''});
+            }
+        }
+        validateUsername();
     }, [username]);
 
     useEffect(() => {
-        if (!email)
-            return;
-        async function checkInput(){
-            await validateEmail(email);
-        }
+        const validateEmail = async () => {
+            const email_input = document.getElementById("email") as HTMLInputElement;
+            const email = email_input.value;
+            
+            if (email) {
+                const res = await fetch('api/validate?email=' + email);
+                const valid = (await res.json()).valid;
+                
+                if (!email_input.validity.valid) {
+                    setEmailError({value: true, message: 'Pattern incorrect'});
+                    email_input.style.borderColor = 'rgb(247, 60, 60)';    
+                }
 
-        checkInput();
+                else if (valid && email_input.value) {
+                    email_input.style.borderColor = 'rgb(247, 60, 60)';
+                    setEmailError({value: true, message: 'Email already exists'});
+                }
+                else {
+                    email_input.style.borderColor = 'rgb(14, 204, 14)';
+                    setEmailError({value: false, message: ''});
+                }
+            }
+            else {
+                email_input.style.borderColor = 'black';
+                setEmailError({value: false, message: ''});
+            }
+        }
+        validateEmail();
     }, [email]);
-    
+
+    useEffect(() => {
+        const validatePassword = async () => {
+            const password_input = document.getElementById("password") as HTMLInputElement;
+            const password = password_input.value;
+            
+            if (password) {
+                
+                if (!password_input.validity.valid) {
+                    setPasswordError({value: true, message: 'Minimum eight characters, at least one letter and one number'});
+                    password_input.style.borderColor = 'rgb(247, 60, 60)';    
+                }
+                else {
+                    password_input.style.borderColor = 'rgb(14, 204, 14)';
+                    setPasswordError({value: false, message: ''});
+                }
+            }
+            else {
+                setPasswordError({value: false, message: ''});
+                password_input.style.borderColor = 'black';
+            }
+        }
+        validatePassword();
+    }, [password]);
+
 
     return (
-            <div className="register-form">
-                
-                <h1>Register</h1>
-                <form action={formAction}>
-                    
-                    <p className='error-message' style={{color: "red"}}>{ state?.message }</p>
-                    <div className="form-input">
-                        <input type="text" id="username" name="username" placeholder=" " required onBlur={(e) => setUsername(e.target.value)}/>
-                        <label htmlFor="username">Username*</label>
-                    </div>
-                    {userError && <p className='error-message' style={{color: "red"}}>Username already exists</p> }
-                    <div className="form-input">
-                        <input type="password" placeholder=" " id="password" name="password" required /> 
-                        <label htmlFor="password">Password*</label>
-                    </div>
-                    <div className="form-input">
-                        <input type="text" id="name" name="name" placeholder=" " required />
-                        <label htmlFor="name">Name*</label>
-                    </div>
-                    <div className="form-input">
-                        <input type="email" placeholder=" " id="email" name="email" required onBlur={(e) => setEmail(e.target.value)}/> 
-                        <label htmlFor="email">Email*</label>
-                        
-                    </div>
-                    {emailError && <p className='error-message' style={{color: "red"}}>Email already exists</p>}
-                    <div className="form-input">
-                        <input type="text" placeholder=" " id="address" name="address" required /> 
-                        <label htmlFor="address">Address*</label>
-                    </div>
-                    <div className="form-input">
-                        <input type="text" placeholder=" " id="tel" name="tel" required /> 
-                        <label htmlFor="tel">Tel.*</label>
-                    </div>
-                    <button type="submit">Submit</button> <br />
-                </form>
+        <div className="register-form">
 
-                <p>Or login with</p>
-                <button type="submit" onClick={() => signIn("google", { callbackUrl: "/" })}>
-                    <Image 
-                        src='/logos/google.png'
-                        alt="Google logo"
-                        width={30}
-                        height={30}
-                    />
-                </button>
-            </div>
+            <h1>Register</h1>
+            <form action={formAction}>
+
+                <p className='error-message' style={{ color: "red" }}>{state?.message}</p>
+                <div className="form-input">
+                    <input type="text" id="username" name="username" placeholder=" " pattern='^[A-Za-z][A-Za-z0-9_]{5,19}$' required onBlur={(e: React.FocusEvent<HTMLInputElement, Element>) => setUsername(e.target.value)} />
+                    <label htmlFor="username">Username*</label>
+                </div>
+                {userError.value && <p className='error-message' id='user_error' style={{ color: "red" }}>{userError.message}</p>}
+                <div className="form-input">
+                    <input type="password" placeholder=" " id="password" name="password" pattern='^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$' required onBlur={(e) => setPassword(e.target.value)}/>
+                    <label htmlFor="password">Password*</label>
+                </div>
+                {passwordError.value && <p className='error-message' style={{ color: "red" }}>{passwordError.message}</p>}
+                <div className="form-input">
+                    <input type="text" id="name" name="name" placeholder=" " required />
+                    <label htmlFor="name">Name*</label>
+                </div>
+                <div className="form-input">
+                    <input type="email" placeholder=" " id="email" name="email" pattern='^[^\s@]+@[^\s@]+\.[^\s@]+' required onBlur={(e) => setEmail(e.target.value)} />
+                    <label htmlFor="email">Email*</label>
+                </div>
+                {emailError.value && <p className='error-message' style={{ color: "red" }}>{emailError.message}</p>}
+                <div className="form-input">
+                    <input type="text" placeholder=" " id="address" name="address" required />
+                    <label htmlFor="address">Address*</label>
+                </div>
+                <div className="form-input">
+                    <input type="text" placeholder=" " id="tel" name="tel" required />
+                    <label htmlFor="tel">Tel.*</label>
+                </div>
+                <button type="submit">Submit</button> <br />
+            </form>
+
+            <p>Or login with</p>
+            <button type="submit" onClick={() => signIn("google", { callbackUrl: "/" })}>
+                <Image
+                    src='/logos/google.png'
+                    alt="Google logo"
+                    width={30}
+                    height={30}
+                />
+            </button>
+        </div>
     )
 }
