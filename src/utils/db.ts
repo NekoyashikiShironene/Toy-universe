4d1c426672911ac379a7afc5c8d159520e18f263
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-23 19:16:39 +0700

    Changed connection: using pool

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,14 +1,17 @@
 import mysql from 'mysql2/promise';
 
-const connectToDatabase = async () => {
-  const connection = await mysql.createConnection({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    idleTimeout: 30000,
   });
-
-  return connection;
+const connectToDatabase = async () => {
+  return pool.getConnection();
 };
 
 export default connectToDatabase;
