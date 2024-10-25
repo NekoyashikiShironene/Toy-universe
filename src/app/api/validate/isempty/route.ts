ef7864b6ec5a82d28a5a5cbd820961fd45b71fae
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-25 19:31:01 +0700

    Added validate empty value route

---
+++ src/app/api/validate/isempty/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectToDatabase from "@/utils/db";
+
+import type { IUser } from "@/types/db";
+
+export async function GET(req: NextRequest) {
+    const id = req.nextUrl.searchParams.get('id');
+    const attribute = req.nextUrl.searchParams.get('attr') || "";
+
+    const connection = await connectToDatabase();
+
+    let results: IUser[] = [];
+    if (id)
+        [results] = await connection.query<IUser[]>("SELECT username, password, name, email, tel FROM customer WHERE cus_id = ? \
+                                                        UNION SELECT username, password, name, email, tel FROM employee WHERE emp_id = ?", [id, id]);
+
+    connection.release();
+
+    const user = (results[0] as IUser)
+    const value = user ? !user[attribute] : true;
+
+    return NextResponse.json({ value });
+}
