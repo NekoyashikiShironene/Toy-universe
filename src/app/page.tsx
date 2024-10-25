33ccdc7a270dcabeede30f2a30b389c6fc41bd34
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-25 19:31:26 +0700

    Removed unused function and imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,53 @@
-import connectToDatabase from "@/utils/db";
 import ImageSlider from "@/components/ImageSlider";
 import Search from "@/components/SearchBar";
-import { ScreenContainer } from "@/components/Containers";
+//import { ScreenContainer } from "@/components/Containers";
 import "../styles/home.css";
-
-type Member = {
-  cus_id: number,
-  username: string,
-  password: string,
-  name: string,
-  tel: string,
-  address: string,
-  gender: number
-}
-
-const testDatabaseConnection = async () => {
-  try {
-    const connection = await connectToDatabase();
-    const [results, fields] = await connection.query("SELECT * FROM `customer`");
-    await connection.end();
-    
-    return results;
-
-
-  } catch (error) {
-    console.error('Database connection failed:', error);
-  }
-};
-
+import Link from "next/link";
+import Image from "next/image";
+import type { Product } from "@/types/products";
+import "../styles/product_card.css";
 
 
 export default async function Home() {
 
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/products?brand=LEGO`);
+  const data = (await res.json()).data;
+  // const product = data[1];
+  
+
   return (
-    <ScreenContainer>
+    <>
       <Search type="mobile" />
       <ImageSlider/>
-    </ScreenContainer>
+      
+
+      <div className="show-product-home">
+        <Link href={`/products?brand=LEGO`}>
+          <div className="topic-product">LEGO</div>
+        </Link>
+        
+        <div className="product-cards">
+          {
+            data.map((product:Product) => (
+                <div key={product.prod_id} className="product-card">
+                <Link href={`/detail?prod_id=${product.prod_id}`}>
+                    <Image src={"/products/" + product.prod_id + ".jpg"}
+                        alt={product.prod_name}
+                        width={300}
+                        height={300}
+                        className="product-img"
+                    />
+                    <p className="product-card-name">{product.prod_name}</p>
+                    <p className="product-card-price">{product.price} บาท</p>
+                </Link>
+            </div>
+
+            ))
+          }
+        </div>
+      </div>
+    </>
+
+    
   );
 }
