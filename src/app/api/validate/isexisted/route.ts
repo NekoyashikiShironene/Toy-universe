9e395162ba9550875c776fe390d47fc9e97d9b4c
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-25 19:30:43 +0700

    Changed api routes path

--- src/app/api/validate/isexisted/route.ts
+++ src/app/api/validate/isexisted/route.ts
@@ -21,7 +21,7 @@ export async function GET(req: NextRequest) {
 
     connection.release();
 
-    const valid = (results.length !== 0);
+    const value = (results.length !== 0);
 
-    return NextResponse.json({ valid });
+    return NextResponse.json({ value });
 }
