b5ce9dbbb9cf8f1ebbd10c665eedbd0c00324590
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-25 19:28:27 +0700

    Made product image in cart can be click to view detail

--- src/components/CartItems.tsx
+++ src/components/CartItems.tsx
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import type { TCartItem } from '../types/products';
 import "../styles/cart.css";
 
@@ -15,15 +16,17 @@ export function CartItem({ item, handleCheck, handleRemove, handleUpdateQuantity
 
  {
   return (
-    <div className="cart-item" onClick={() => { }}>
+    <div className="cart-item">
       <input className='item-checkbox' type="checkbox" checked={item.checked} onChange={(e) => handleCheck(item, e.target.checked)} />
-      <Image
-        src={`/products/${item.prod_id}.jpg`}
-        alt={item.prod_name}
-        width={100}
-        height={100}
-        className="item-image"
-      />
+      <Link href={`/detail?prod_id=${item.prod_id}`}>
+        <Image
+          src={`/products/${item.prod_id}.jpg`}
+          alt={item.prod_name}
+          width={100}
+          height={100}
+          className="item-image"
+        />
+      </Link>
       <div className="item-info">
         <h2 className="item-name">{item.prod_name}</h2>
         <div className="item-price-manager">
