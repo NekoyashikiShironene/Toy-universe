92e9ca2de7f7a3be33802735c9034f49342c8270
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-25 17:46:39 +0700

    Converted client page to server page

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -1,93 +1,24 @@
-"use client";
-
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Image from 'next/image';
-import type { Product } from '../../types/products';
 import "../../styles/detail.css";
-import Link from 'next/link';
-import { useSession } from "next-auth/react";
-import { UserSession } from '@/types/session';
-import { useRouter } from 'next/navigation';
+import ProductOrderControls from '@/components/ProductOrderControls';
 
 type Prop = {
   searchParams: { [key: string]: string | string[] | undefined }
 }
 
-export default function ProductDetailPage({ searchParams }: Prop) {
-  const { data: session, update } = useSession();
-  const router = useRouter();
+export default async function ProductDetailPage({ searchParams }: Prop) {
+  const productId = searchParams.prod_id as string;
 
-  const productIdParam = searchParams.prod_id as string; // ดึง prod_id จาก query params
-  const prod_id = parseInt(productIdParam);
-
-  const [product, setProduct] = useState<Product>({
-    prod_id: 0,       
-    prod_name: "",     
-    category: "",   
-    brand: "",         
-    description: "",    
-    price: 0,         
-    remaining: 0      
-});
-  const [quantity, setQuantity] = useState<number>(1);
-
-  useEffect(() => {
-    const getProductDetails = async (prod_id: number) => {
-      try {
-        const res = await fetch("api/product?prod_ids=" + prod_id);
-        const data = (await res.json()).data;
-        setProduct(data[0]);
-
-      } catch (error) {
-        console.error('Database connection failed:', error);
-      }
-    };
-    getProductDetails(prod_id);
-  }, [prod_id]);
-
-  const handlePlus = () => {
-    if (quantity < product.remaining) {
-      setQuantity(quantity + 1);
-    }
-  }
-
-  const handleMinus = () => {
-    if (quantity > 1) {
-      setQuantity(quantity - 1);
-    }
-  }
-
-  const addProductToCart = () => {
-    const newProduct = {
-      id: prod_id,
-      quantity: quantity
-    };
-    const user = session?.user as UserSession;
-    const prevCart = user.cart;
-
-    const isItemInCart = prevCart.find((cartItem) => cartItem.id === prod_id)
-    if (isItemInCart) {
-      update({
-        cart: prevCart.map((cartItem) =>
-          cartItem.id === prod_id
-            ? { ...cartItem, quantity: cartItem.quantity + quantity }
-            : cartItem
-        )
-      });
-    } else {
-      update({
-        cart: [...prevCart, newProduct]
-      });
-    }
-
-    router.push('/cart');
-  }
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/product?prod_ids=${productId}`);
+  const data = (await res.json()).data;
+  const product = data[0];
 
   return (
     <div className="product-detail">
       <div className="image-container">
         <Image
-          src={`/products/${prod_id}.jpg`}
+          src={`/products/${productId}.jpg`}
           alt={product.prod_name}
           width={300}
           height={300}
@@ -96,20 +27,10 @@ export default function ProductDetailPage({ searchParams }: Prop) {
       </div>
       <div className="details">
         <h1 className="product-name">{product.prod_name}</h1>
-        <p className="product-price">Price: <span className="price">{product.price} บาท</span></p>
+        <p className="product-price">Price: <span className="price">{product.price} ฿</span></p>
         <p className="product-description">Detail: <br />{product.description}</p>
         <p className="stock">Remaining: {product.remaining} pieces</p>
-        <div className="item-quantity">
-          <button onClick={handleMinus}>-</button>
-          <input type="number" value={quantity} min={1} max={product.remaining} onChange={(e) => setQuantity(Math.min(product.remaining, parseInt(e.target.value)))}/>
-          <button onClick={handlePlus}>+</button>
-        </div>
-        <button className="add-to-cart" onClick={addProductToCart} disabled={product.remaining === 0}>Add to Cart</button>
-        <Link href={"/payment?prod_id=" + product.prod_id}>
-          <button type="submit" className="buy" disabled={product.remaining === 0}>
-            Buy now
-          </button>
-        </Link>
+        <ProductOrderControls product={product} />
       </div>
     </div>
   );
