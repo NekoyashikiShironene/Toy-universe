260e59e6cd8aa4d637e37d21dd4c6b1c2273d511
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-29 20:17:25 +0700

    Changed type of searchParams

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { ContentContainer } from '@/components/Containers';
 import { ProductCards } from '@/components/ProductCard';
+import  ProductFilter  from '@/components/ProductFilter';
 
 type Prop = {
     searchParams: { [key: string]: string | string[] | undefined }
 }
 
 export default async function ProductsPage({ searchParams }: Prop) {
-    const query = searchParams.query as string;
-    const category = searchParams.category as string;
-    const brand = searchParams.brand as string;
+    const query = searchParams.query as string | undefined;
+    const category = searchParams.category as string | undefined;
+    const brand = searchParams.brand as string | undefined;
 
-    
     const url = new URL(`${process.env.NEXT_PUBLIC_URL}/api/products`);
 
     if (query)
@@ -23,12 +23,13 @@ export default async function ProductsPage({ searchParams }: Prop) {
     if (brand)
         url.searchParams.set("brand", brand);
 
-    const res = await fetch(url.toString());
+    const res = await fetch(url.toString(), {cache: "no-store"});
     const products = await res.json();
     
     return (
         // $_GET["query"]
-        <>
+        <>  
+            <ProductFilter/>
             <ProductCards products={products.data} />
         </>
     )
