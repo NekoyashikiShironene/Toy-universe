669500397c6f8ddaeb93e756cee4695547e6c260
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-10-29 20:18:27 +0700

    Used singleton pattern for database connection

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,17 +1,37 @@
-import mysql from 'mysql2/promise';
+import mysql, { PoolConnection } from 'mysql2/promise';
 
-const pool = mysql.createPool({
+
+let globalPool: PoolConnection | undefined = undefined;
+
+export default async function connectToDatabase() {
+  if (globalPool !== undefined)
+    return globalPool;
+
+  const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    idleTimeout: 30000,
+  //  waitForConnections: true,
+  //  connectionLimit: 10,
+  //  queueLimit: 0,
+  //  idleTimeout: 30000,
   });
-const connectToDatabase = async () => {
-  return pool.getConnection();
+  
+  console.log("Created new connection");
+  globalPool = await pool.getConnection();
+
+  return globalPool;
 };
 
-export default connectToDatabase;
+
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  idleTimeout: 30000,
+});
