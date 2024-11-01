b1c8ed1f88892cd9241336df47104268c357b703
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-01 23:07:22 +0700

    Implemented fetching logic

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -8,28 +8,52 @@ type Prop = {
 }
 
 export default async function ProductsPage({ searchParams }: Prop) {
-    const query = searchParams.query as string | undefined;
-    const category = searchParams.category as string | undefined;
-    const brand = searchParams.brand as string | undefined;
+    const query = searchParams.query ?? "" as string;
+    const category = searchParams.category ?? [] as string[];
+    const brand = searchParams.brand ?? [] as string[];
+    const maxPrice = searchParams.maxPrice as string;
 
     const url = new URL(`${process.env.NEXT_PUBLIC_URL}/api/products`);
 
-    if (query)
+    if (query) {
         url.searchParams.set("query", query);
+    }
 
-    if (category)
-        url.searchParams.set("category", category);
+    // Set multiple category values if present
+    if (category.length > 0) {
+        if (typeof category === "string")
+            url.searchParams.append("category", category);
+        else
+            category.forEach(cat => url.searchParams.append("category", cat));
+    }
 
-    if (brand)
-        url.searchParams.set("brand", brand);
+    // Set multiple brand values if present
+    if (brand.length > 0) {
+        if (typeof brand === "string")
+            url.searchParams.append("brand", brand);
+        else
+            brand.forEach(br => url.searchParams.append("brand", br));
+    }
 
-    const res = await fetch(url.toString(), {cache: "no-store"});
-    const products = await res.json();
+    // Add maxPrice if it exists
+    if (maxPrice) {
+        url.searchParams.set("maxPrice", maxPrice);
+    }
+
+
+        const res = await fetch(url.toString(), { cache: "no-store" });
+        
+
+
+        const products = await res.json();
+
     
+    
+
     return (
         // $_GET["query"]
         <>  
-            <ProductFilter/>
+            <ProductFilter category={category} brand={brand} maxPrice={maxPrice} />
             <ProductCards products={products.data} />
         </>
     )
