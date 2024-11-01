c0673bb1d7c133428c383ade14bcb11b9f55a690
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-01 23:07:46 +0700

    Completed product filter

--- src/components/ProductFilter.tsx
+++ src/components/ProductFilter.tsx
@@ -1,46 +1,107 @@
-export default function ProductFilter() {
+"use client";
 
-    const categories = ['Construction', 'Doll', 'Model', 'Board Game'];
-    const brands = ['LEGO', 'Mattel', 'American Girl', 'Disney', 'LOL Surprise', 'Hasbro', 'Bandai', 'Revell', 'Tamiya', 'Metal Earth', 'KOSMOS', 'Days of Wonder', 'Z-Man Games'];
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { categories, brands } from "@/utils/array";
+
+
+import "../styles/productFilter.css";
+
+export default function ProductFilter({ category, brand, maxPrice }: { category: string[], brand: string[], maxPrice: string | undefined }) {
+    const router = useRouter();
+
+    const [maxPriceState, setMaxPriceState] = useState<number>(Number(maxPrice) || 10000 );
+
+    const redirect = (name: string, value: string, checked: boolean) => {
+        const url = new URL(`${process.env.NEXT_PUBLIC_URL}/products`);
+
+        if (typeof category === "string")
+            url.searchParams.append("category", category);
+        else
+            category.forEach(category => url.searchParams.append("category", category));
+
+        if (typeof brand === "string")
+            url.searchParams.append("brand", brand);
+        else
+            brand.forEach(brand => url.searchParams.append("brand", brand));
+
+        if (maxPrice)
+            url.searchParams.set("maxPrice", maxPrice);
+
+        if (name === "maxPrice") {
+            url.searchParams.set("maxPrice", value);
+            router.push(url.toString());
+            return;
+        }
+            
+
+        if (checked)
+            url.searchParams.append(name, value);
+        else
+            url.searchParams.delete(name, value);
+
+        router.push(url.toString());
+    }
+
 
     return (
         <>
-            <h1>ProductFilter</h1>
+        <div className="filter">
+            
+            <div className="product-filter">
+            
+                <div className="product-filter-categorie">
+                {
+                    
+                    categories.map((item: string) => (
+                        <>
+                            <div className="each-filter-content">
+                                <input 
+                                    
+                                    type="checkbox"
+                                    onChange={(e) => redirect("category", item, e.currentTarget.checked)}
+                                    checked={category.includes(item)}
+                                />
+                                <label>{item}</label>
+                            </div>
+                        </>
 
-            <div>
-            {
-                // categories.map(item => (
+                    ))
+                }
+                </div>
 
-                // ))
-               
-                categories.map((item: string) => (
-                    <>
-                        <input type="checkbox"/>
-                        <label>{item}</label>
-                    </>
+                <div className="product-filter-brands">
+                {
+                    
+                    brands.map((item: string) => (
+                        <>
+                            <div className="each-filter-content">
+                                <input
+                                    type="checkbox" 
+                                    onChange={(e) => redirect("brand", item, e.currentTarget.checked)}
+                                    checked={brand.includes(item)}
+                                />
+                                <label>{item}</label>
+                            </div>
+                        </>
 
-                ))
-            }
+                    ))
+                }
+                </div>
+            
+                <label htmlFor="">price</label>
+                <input 
+                    type="range" 
+                    min={500} 
+                    max={10000} 
+                    step={500} 
+                    value={maxPriceState} 
+                    onMouseUp={e => redirect("maxPrice", e.currentTarget.value.toString(), true)}
+                    onChange={e => setMaxPriceState(Number(e.currentTarget.value))}
+                />
+                <label className="filter-price"> {maxPriceState} บาท</label>
             </div>
-
-            <div>
-            {
-                // categories.map(item => (
-
-                // ))
-               
-                brands.map((item: string) => (
-                    <>
-                        <input type="checkbox"/>
-                        <label>{item}</label>
-                    </>
-
-                ))
-            }
-            </div>
-
-            <label htmlFor="">price</label>
-            <input type="range" />
-        </>
+        </div>
+    </>
     );
 }
