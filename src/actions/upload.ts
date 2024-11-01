0cddef38cf3e6b74e244542f91df1aa3f0431521
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-02 01:55:32 +0700

    Removed addProduct

--- src/actions/upload.ts
+++ src/actions/upload.ts
@@ -14,32 +14,4 @@ export async function uploadProfilePicture(formData: FormData) {
     return result;
 }
 
-export async function addProduct(formData: FormData) {
-    const prod_name = formData.get('prod_name') as string;
-    const category = formData.get('category') as string;
-    const brand = formData.get('brand') as string;
-    const description = formData.get('description') as string;
-    const price = formData.get('price') as string;
-    const stock = parseInt(formData.get('stock') as string);
-    const image = formData.get("product-pic") as File;
-    
-    const connection = await connectToDatabase();
 
-    try {
-        const [ results ] = await connection.query("INSERT INTO product (prod_name, category, brand, description, price, remaining) \
-                                                VALUES(?, ?, ?, ?, ?, ?)", [prod_name, category, brand, description, price, stock]);
-
-        if (image) {
-            const result = await uploadFile(image, results.insertId, "/products");
-        }
-        
-        
-    } catch (e: unknown) {
-        console.error(e);
-        return {
-            message: "Add Product failed"
-        }
-    }
-
-    redirect("/products"); 
-}
