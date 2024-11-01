0718c54a6f1a6bee67302d955d2d36f599294a53
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-02 01:59:48 +0700

    Added preview image component

---
+++ src/components/PreviewImage.tsx
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import Image from 'next/image';
+import { useState } from 'react';
+
+export default function PreviewImage() {
+    const [image, setImage] = useState(null);
+
+    const showPreviewImage = (e) => {
+        const file = e?.target.files[0];
+        if (file) {
+            setImage(URL.createObjectURL(file));
+        }
+        else {
+            setImage(null);
+        }
+    }
+
+    return (
+        <>
+            <div className='product-input'>
+                <label>Picture:</label>
+                <input type='file' name='product-pic' id='product-pic' onChange={showPreviewImage} />
+            </div>
+            
+            { image && <Image src={image} className='preview-image' alt={''} width={150} height={150}/>}
+        </> 
+    )
+        
+}
