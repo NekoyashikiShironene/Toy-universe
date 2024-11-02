8da4add7b9be5b7237adfa4faf21c1b1a09e8c35
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-02 17:38:09 +0700

    Added address type

---
+++ src/types/address.ts
@@ -0,0 +1,9 @@
+export type Address = {
+    house_number: string | null,
+    street: string | null,
+    subdistrict: string | null,
+    district: string | null,
+    province: string | null,
+    postal_code: string | null,
+    country: string | null
+}
