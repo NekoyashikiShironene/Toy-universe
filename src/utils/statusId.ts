1b342dc05f71c3bf5bdb69eded2bef865e8d95ea
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-02 17:38:22 +0700

    Added status id converter

---
+++ src/utils/statusId.ts
@@ -0,0 +1,9 @@
+export const orderStatus = {
+    0: "Pending Payment", //auto
+    1: "Payment Completed", //auto
+    2: "Shipped",
+    3: "Completed",
+    4: "Cancelled",
+    5: "",
+    6: "",
+}
