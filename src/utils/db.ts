02151f9490c959bc8773be809e286f63a12fd53f
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-03 02:54:47 +0700

    Uncomment config

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -12,10 +12,10 @@ export default async function connectToDatabase() {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-  //  waitForConnections: true,
-  //  connectionLimit: 10,
-  //  queueLimit: 0,
-  //  idleTimeout: 30000,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    idleTimeout: 30000,
   });
   
   console.log("Created new connection");
