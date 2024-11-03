fb2c326ac52b9e767837efb8283627ce65614f63
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-04 01:59:45 +0700

    Used ContentContainer

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import "../../styles/detail.css";
 import ProductOrderControls from '@/components/ProductOrderControls';
+import { ContentContainer } from '@/components/Containers';
 
 type Prop = {
   searchParams: { [key: string]: string | string[] | undefined }
@@ -15,24 +16,26 @@ export default async function ProductDetailPage({ searchParams }: Prop) {
   const product = data[0];
 
   return (
-    <div className="product-detail">
-      <div className="image-container">
-        <Image
-          src={`/products/${productId}.jpg`}
-          alt={product.prod_name}
-          width={300}
-          height={300}
-          className="product-image"
-        />
+    <ContentContainer>
+      <div className="product-detail">
+        <div className="image-container">
+          <Image
+            src={`/products/${productId}.jpg`}
+            alt={product.prod_name}
+            width={300}
+            height={300}
+            className="product-image"
+          />
+        </div>
+        <div className="details">
+          <h1 className="product-name">{product.prod_name}</h1>
+          <p className="product-price">Price: <span className="price">{product.price} ฿</span></p>
+          <p className="product-description">Detail: <br />{product.description}</p>
+          <p className="stock">Remaining: {product.remaining} pieces</p>
+          <ProductOrderControls product={product} />
+        </div>
       </div>
-      <div className="details">
-        <h1 className="product-name">{product.prod_name}</h1>
-        <p className="product-price">Price: <span className="price">{product.price} ฿</span></p>
-        <p className="product-description">Detail: <br />{product.description}</p>
-        <p className="stock">Remaining: {product.remaining} pieces</p>
-        <ProductOrderControls product={product} />
-      </div>
-    </div>
+    </ContentContainer>
   );
 }
 
