865af13ba29600a8ff43fa888ac6b80999b62d83
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-04 01:50:41 +0700

    Added back button

---
+++ src/components/BackButton.tsx
@@ -0,0 +1,22 @@
+'use client'
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import { IoMdArrowRoundBack } from "react-icons/io";
+
+export default function BackButton() {
+    const router = useRouter();
+
+    const handleBack = () => {
+        router.back(); 
+    };
+
+    return (
+        <div>
+            
+            <button onClick={handleBack} className='back-button'>
+                <IoMdArrowRoundBack size={40} />
+            </button>
+        </div>
+    )
+}
+
