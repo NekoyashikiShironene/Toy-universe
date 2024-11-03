405fb42234c10691571cdee9888464539c3c31b1
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-04 01:50:00 +0700

    Used cache

--- src/app/order-management/page.tsx
+++ src/app/order-management/page.tsx
@@ -9,7 +9,9 @@ import OrderStatusDropdown from '@/components/OrderStatusDropdown';
 import OrderItem from '@/components/OrderItem';
 import useSession from '@/utils/auth';
 import { UserSession } from '@/types/session';
-import {redirect} from "next/navigation";
+import { redirect } from "next/navigation";
+import StatusFilter from '@/components/OrderFilter';
+import { unstable_cache } from 'next/cache';
 
 const formatAddress = (address: Address): string => {
     
@@ -24,55 +26,79 @@ const formatAddress = (address: Address): string => {
     ].filter(Boolean).join(' ');
 }
 
-export default async function OrderManagement() {
-    const user = (await useSession())?.user as UserSession;
+type Props = {
+    searchParams: { [key: string]: string | undefined }
+}
 
-    if (user.role !== "emp")
+
+const getOrder = unstable_cache(
+    async (statusFilter: string | string[] | undefined, user: UserSession) => { 
+        const connection = await connectToDatabase();
+        let orders;
+        try {
+            const [ results ] = await connection.query( `SELECT DISTINCT order.ord_id, status_id, shipping_address, date_time, order_item.cus_id, name, email, tel 
+                                                        FROM \`order\` 
+                                                        JOIN order_item ON \`order\`.ord_id = order_item.ord_id 
+                                                        JOIN customer ON order_item.cus_id = customer.cus_id 
+                                                        ${statusFilter ? 'WHERE status_id = ?' : ''}`, 
+                                                        statusFilter ? [statusFilter] : []);
+            orders = results as Order[];
+            
+        }   
+        catch (e: unknown) {
+            console.error(e);
+        }
+        
+        return orders;
+    },
+    ["customer-order"],
+    { revalidate: 3600 }
+)
+
+
+export default async function OrderManagement({ searchParams }: Props) {
+    const user = (await useSession())?.user as UserSession;
+    const statusFilter = searchParams.status_id;
+
+    if (!user || user.role !== "emp")
         redirect("/");
     
-    const connection = await connectToDatabase();
-    let orders;
-    try {
-        const [ results ] = await connection.query("SELECT DISTINCT order.ord_id, status_id, shipping_address, date_time, order_item.cus_id, name, email, tel \
-                                                    FROM `order` JOIN order_item ON `order`.ord_id = order_item.ord_id \
-                                                    JOIN customer ON order_item.cus_id = customer.cus_id");
-        orders = results as Order[];
-    }   
-    catch (e: unknown) {
-        console.error(e);
-    }
+    const orders = await getOrder(statusFilter, user);
 
     return (
         <ContentContainer>
             { user.role === 'emp' ?
+                <>
+                <StatusFilter selectedStatus={statusFilter || ''} />
                 <div className='order-list'>
                     {
                         orders?.map(order => (
                             <div key={order.ord_id} className='order-container'>
-                                <div className='order-row'>
-                                    <span>Order ID: {order.ord_id}</span>
-                                    <span>Date: {order.date_time.toLocaleDateString()}</span>
-                                    <span>Customer ID: {order.cus_id}</span>
+                                <div className='order-header'>
+                                    <span><b>Order ID:</b> {order.ord_id}</span>
+                                    <span><b>Date:</b> {order.date_time.toString()}</span>
+                                    
+                                    <span><b>Status:</b> {orderStatus[order.status_id]}</span>
                                 </div>
 
                                 <div className='order-row'>
-                                    <span>Customer: {order.name}</span>
-                                    <span>Shipping Address: {formatAddress(order.shipping_address)}</span>
+                                    <span><b>Customer ID:</b> {order.cus_id}</span>
+                                    <span><b>Customer:</b> {order.name}</span>
+                                    <span><b>Shipping Address:</b> {formatAddress(order.shipping_address)}</span>
+                                    <span><b>Email:</b> {order.email}</span>
+                                    <span><b>Tel.:</b> {order.tel}</span>
                                 </div>
 
-                                <div className='order-row'>
-                                    <span>Email: {order.email}</span>
-                                    <span>Tel.: {order.tel}</span>
-                                </div>
-                                
                                 <OrderItem orderId={order.ord_id} />
                                 <OrderStatusDropdown order={order} />
                             </div>
                         ))
                     }
-                </div>
+                    </div>
+                </>
                 : 
-                <h1>You don't have permission for this page.</h1>
+                <h1>You don&apos;t have permission for this page.</h1>
+            
         }
         </ContentContainer>
     )
--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -3,67 +3,77 @@ import connectToDatabase from "@/utils/db";
 import { ContentContainer } from '@/components/Containers';
 import '@/styles/order.css';
 import type { Order } from '@/types/order';
-import type { Address } from '@/types/address';
 import { orderStatus } from '@/utils/statusId';
-import OrderStatusDropdown from '@/components/OrderStatusDropdown';
 import { UserSession } from '@/types/session';
 import { redirect } from "next/navigation";
 import useSession from '@/utils/auth';
 import { formatAddress } from '@/utils/address';
 import OrderItem from '@/components/OrderItem';
 import OrderManager from '@/components/OrderManager';
+import StatusFilter from '@/components/OrderFilter';
 
-export default async function Order() {
+import { unstable_cache } from 'next/cache';
+
+type Props = {
+    searchParams: { [key: string]: string | string[] | undefined }
+}
+
+const getOrder = unstable_cache(
+    async (statusFilter: string | string[] | undefined, user: UserSession) => {
+        const connection = await connectToDatabase();
+        const [ results ] = await connection.query(`SELECT DISTINCT order.ord_id, status_id, shipping_address, order.session_id, 
+            order.session_url, date_time, order_item.cus_id, name, email, tel 
+            FROM \`order\`
+            JOIN order_item ON order.ord_id = order_item.ord_id 
+            JOIN customer ON order_item.cus_id = customer.cus_id 
+            WHERE order_item.cus_id = ? 
+            ${statusFilter ? 'AND status_id = ?' : ''}
+            ORDER BY order.ord_id DESC`,
+            statusFilter ? [user.id, statusFilter] : [user.id]
+        );
+
+        const orders = results as Order[];
+        return orders;
+    },
+    ["my-order"],
+    { revalidate: 3600 }
+)
+
+export default async function Order({ searchParams }: Props) {
     const user = (await useSession())?.user as UserSession;
+    const statusFilter = searchParams.status_id;
 
     if (!user)
         redirect("/login");
     
-    const connection = await connectToDatabase();
-
-    const [ results ] = await connection.query(
-        "SELECT DISTINCT status_id, `order`.ord_id, status_id, shipping_address, `order`.session_id, `order`.session_url, \
-        date_time, order_item.cus_id, name, email, tel \
-        FROM `order` JOIN order_item ON `order`.ord_id = order_item.ord_id \
-        JOIN customer ON order_item.cus_id = customer.cus_id WHERE order_item.cus_id = ? \
-        ORDER BY order.ord_id DESC", 
-        [user.id]
-    );
-
-    const orders = results as Order[];
-
-    console.log(results);
+    const orders = await getOrder(statusFilter, user);
     
     return (
         <ContentContainer>
-
-            <div className='order-item-container'>
-            
-                {
-                    orders?.map(order => (
-                        <div key={order.ord_id} className='order-container'>
-                            <div className='order-row'>
-                                <span>Order ID: {order.ord_id}</span>
-                                <span>Status: {orderStatus[order.status_id]}</span>
-                                <span>Date: {order.date_time.toLocaleDateString()}</span>
-                                <span>Customer ID: {order.cus_id}</span>
-                            </div>
-
-                            <div className='order-row'>
-                                <span>Customer: {order.name}</span>
-                                <span>Shipping Address: {formatAddress(order.shipping_address)}</span>
-                            </div>
-
-                            <div className='order-row'>
-                                <span>Email: {order.email}</span>
-                                <span>Tel.: {order.tel}</span>
-                            </div>
-                            
-                            <OrderItem orderId={order.ord_id} />
-                            <OrderManager orderStatus={order.status_id} session_id={order.session_id} session_url={order.session_url} />
+            <StatusFilter selectedStatus={statusFilter || ''} />
+            <div className='order-list'>
+            {
+                orders?.map(order => (
+                    <div key={order.ord_id} className='order-container'>
+                        <div className='order-header'>
+                            <span><b>Order ID:</b> {order.ord_id}</span>
+                            <span><b>Status:</b> {orderStatus[order.status_id]}</span>
+                            <span><b>Date:</b> {order.date_time.toString()}</span>
                         </div>
-                    ))
-                }
+
+                        <div className='order-row'>
+                            <span><b>Customer ID:</b> {order.cus_id}</span>
+                            <span><b>Customer:</b> {order.name}</span>
+                            <span><b>Shipping Address:</b> {formatAddress(order.shipping_address)}</span>
+                            <span><b>Email:</b> {order.email}</span>
+                            <span><b>Tel.:</b> {order.tel}</span>
+                        </div>
+
+                        <OrderItem orderId={order.ord_id} />
+                        <OrderManager orderStatus={order.status_id} session_id={order.session_id} session_url={order.session_url} />
+                    </div>
+                ))
+            }
             </div>
 
         </ContentContainer>
