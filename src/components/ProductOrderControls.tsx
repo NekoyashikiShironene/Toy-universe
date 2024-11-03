78145f61feb48f52f491390c76f37e1eaaf21096
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-04 01:58:23 +0700

    Applied new cart system

--- src/components/ProductOrderControls.tsx
+++ src/components/ProductOrderControls.tsx
@@ -13,20 +13,23 @@ export default function ProductOrderControls({ product }: { product: Product }) 
     const router = useRouter();
     const [quantity, setQuantity] = useState<number>(1);
     
-    const addProductToCart = () => {
+    const addProductToCart = (checked: boolean) => {
         const isItemInCart = cartItems.find((cartItem) => cartItem.prod_id === product.prod_id)
         if (isItemInCart) {
           setCartItems(cartItems.map((cartItem) =>
               cartItem.prod_id === product.prod_id
-                ? { ...cartItem, quantity: cartItem.quantity + quantity }
+                ? { ...cartItem, quantity: cartItem.quantity + quantity, checked }
                 : cartItem
             )
           );
         } else {
-          setCartItems([...cartItems, {...product, quantity}]);
+          setCartItems([...cartItems, {...product, quantity, checked}]);
         }
-    
-        router.push('/cart');
+
+        if (checked)
+          router.push('/payment');
+        else
+          router.push('/cart');
       }
 
     return (
@@ -35,12 +38,23 @@ export default function ProductOrderControls({ product }: { product: Product }) 
             <div className="item-quantity">
                 <input type="number" value={quantity} min={1} max={product.remaining} onChange={(e) => setQuantity(Math.min(product.remaining, parseInt(e.target.value)))} />
             </div>
-            <button className="add-to-cart" onClick={addProductToCart} disabled={product.remaining === 0}>Add to Cart</button>
-            <Link href={"/payment?prod_id=" + product.prod_id}>
-                <button type="submit" className="buy" disabled={product.remaining === 0}>
-                    Buy now
-                </button>
-            </Link>
+            
+            <button 
+              className="add-to-cart" 
+              onClick={() => addProductToCart(false)} 
+              disabled={product.remaining === 0}
+            >
+              Add to Cart
+            </button>
+
+            <button 
+              className="buy" 
+              disabled={product.remaining === 0}
+              onClick={() => addProductToCart(true)}
+            >
+                Buy now
+            </button>
+
         </>
 
     )
