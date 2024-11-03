d8a122deba454947b88bd3489d278ec9233a3a1f
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-04 01:58:45 +0700

    Added back button to content container

--- src/components/Containers.tsx
+++ src/components/Containers.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import BackButton from './BackButton';
 
 type TContainer = {
     children?: React.ReactNode,
@@ -9,14 +10,17 @@ type TContainer = {
 export function ScreenContainer({ children, className, ...rest }: TContainer) {
     return (
         <div className={`screen-container ${className ?? ""}`} {...rest} >
+            
             {children}
         </div>
     )
 }
 
 export function ContentContainer({ children, className, ...rest }: TContainer) {
+
     return (
         <article className={`content-container ${className ?? ""}`} {...rest} >
+            <BackButton />
             {children}
         </article>
     )
