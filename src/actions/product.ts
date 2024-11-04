3a37f4dc48faf03f58553607d1d83485092872cd
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-04 17:28:08 +0700

    Used imported db quey

--- src/actions/product.ts
+++ src/actions/product.ts
@@ -1,61 +1,97 @@
 "use server";
 import connectToDatabase from "@/utils/db";
+import { addProduct } from "@/db/product";
 import { uploadFile } from "@/utils/uploadFile";
-import { ResultSetHeader } from "mysql2";
-import { redirect } from "next/navigation";
+import { updateProduct } from "@/db/product";
+import { revalidatePath } from "next/cache";
 
-export async function addProduct(formData: FormData) {
+export async function addProductAction(prevState: unknown, formData: FormData) {
     const prod_name = formData.get('prod_name') as string;
     const category = formData.get('category') as string;
     const brand = formData.get('brand') as string;
     const description = formData.get('description') as string;
-    const price = formData.get('price') as string;
+    const price = parseFloat(formData.get('price') as string);
     const stock = parseInt(formData.get('stock') as string);
     const image = formData.get("product-pic") as File;
     
-    const connection = await connectToDatabase();
+    if (stock < 0) {
+        return {
+            message: "Add Product Failed, stock can't be negative",
+            success: false
+        }
+    }
+    else if (price < 0) {
+        return {
+            message: "Add Product Failed, price can't be negative",
+            success: false
+        }
+    }
+
 
     try {
-        const [ results ] = await connection.query<ResultSetHeader>("INSERT INTO product (prod_name, category, brand, description, price, remaining) \
-                                                VALUES(?, ?, ?, ?, ?, ?)", [prod_name, category, brand, description, price, stock]);
+        const result = await addProduct(prod_name, category, brand, description, price, stock);
 
         if (image) {
-            await uploadFile(image, results.insertId.toString(), "/products");
+            await uploadFile(image, result.insertId.toString(), "/products");
         }
+
+        revalidatePath("/products");
+        revalidatePath("/detail");
         
+        return {
+            message: "Add Product Successful",
+            success: true
+        }    
         
     } catch (e: unknown) {
         console.error(e);
         return {
-            message: "Add Product failed"
+            message: "Add Product Failed",
+            success: false
         }
-    }
-
-    redirect("/products"); 
+    } 
 }
 
-export async function updateProduct(formData: FormData) {
+export async function updateProductAction(prevState: unknown, formData: FormData) {
     const prod_id = formData.get('prod_id') as string;
     const prod_name = formData.get('prod_name') as string;
     const category = formData.get('category') as string;
     const brand = formData.get('brand') as string;
     const description = formData.get('description') as string;
-    const price = formData.get('price') as string;
+    const price = parseFloat(formData.get('price') as string);
     const remaining = parseInt(formData.get('remaining') as string);
     const image = formData.get("product-pic") as File;
     
-    const connection = await connectToDatabase();
+    if (remaining < 0) {
+        return {
+            message: "Add Product Failed, stock can't be negative",
+            success: false
+        }
+    }
+    else if (price < 0) {
+        return {
+            message: "Add Product Failed, price can't be negative",
+            success: false
+        }
+    }
 
     try {
-       await connection.query("UPDATE product SET prod_name = ?, category = ?, brand = ?, description = ?, price = ?, remaining = ?\
-                                    WHERE prod_id = ?" , [prod_name, category, brand, description, price, remaining, prod_id]);
+        await updateProduct(prod_name, category, brand, description, price, remaining, prod_id);
         if (image) {
             await uploadFile(image, prod_id, "/products");
+        }
+
+        revalidatePath("/products");
+        revalidatePath("/detail");
+        return {
+            message: "Update Product Successful",
+            success: true
         }
     } catch (e: unknown) {
         console.error(e);
         return {
-            message: "Update Product failed"
+            message: "Update Product failed",
+            success: false
         }
     }
 }
