f86bd41870746361a28d2e5d47ef6b4e5fadb017
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-04 22:10:46 +0700

    Added update password

--- src/actions/updateAccount.ts
+++ src/actions/updateAccount.ts
@@ -2,6 +2,8 @@
 
 import { redirect } from "next/navigation";
 import connectToDatabase from "@/utils/db";
+import NextCrypto from "next-crypto";
+
 
 export async function updateAccount(formData: FormData) {
 
@@ -11,19 +13,57 @@ export async function updateAccount(formData: FormData) {
     const email = formData.get('email') as string;
     const tel = formData.get('tel') as string;
     const address = formData.get('address') as string;
-
-    //const key = process.env.CRYPTO_SECRET as string;
-    //const crypto = new NextCrypto(key);
-    //const encryptedPassword = await crypto.encrypt(password);
     
     const connection = await connectToDatabase();
 
     try {
         await connection.query("UPDATE customer SET username = ?, name = ?, email = ?, tel = ?, address = ? WHERE cus_id = ?", [username, name, email, tel, address, id]);
-    } catch (e: unknown) {
+        return {
+            message: "Update user account successful"
+        }
+    } 
+    catch (e: unknown) {
         console.error(e);
         return {
             message: "Update user account failed"
+        }
+    }
+
+    redirect("/profile");
+
+}
+
+export async function changePassword(prevState: unknown, formData: FormData) {
+
+    const id = formData.get('id') as string;
+    const pwd1 = formData.get('password1') as string;
+    const pwd2 = formData.get('password2') as string;
+
+    if (pwd1 !== pwd2) {
+        return {
+            message: "password isn't match"
+        }
+    }
+
+    const key = process.env.CRYPTO_SECRET as string;
+    const crypto = new NextCrypto(key);
+    const encryptedPassword = await crypto.encrypt(pwd1);
+    
+    const connection = await connectToDatabase();
+
+    try {
+        await connection.query("UPDATE customer SET password = ? WHERE cus_id = ?", [encryptedPassword, id]);
+        await connection.query("UPDATE employee SET password = ? WHERE emp_id = ?", [encryptedPassword, id]);
+        return {
+            message: "Update user password successful",
+            success: true
+        }
+
+    } catch (e: unknown) {
+        console.error(e);
+        return {
+            message: "Update user password failed",
+            success: false
         }
     }
 
