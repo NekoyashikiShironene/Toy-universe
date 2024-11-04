ef13ade273d1e82a14ff1773c153ae84bb907b08
NekoyashikiShironene <s6504062663095@email.kmutnb.ac.th>
2024-11-04 17:29:16 +0700

    Fixed preview image doesn't show after adding product next time

--- src/components/PreviewImage.tsx
+++ src/components/PreviewImage.tsx
@@ -1,22 +1,31 @@
 "use client";
+
 import React from "react";
 import Image from 'next/image';
 import { useState } from 'react';
+import '@/styles/preview-image.css';
 
-export default function PreviewImage() {
+export default function PreviewImage({showImage, setShowImage}: 
+    {
+        showImage: boolean, 
+        setShowImage: React.Dispatch<React.SetStateAction<boolean>>
+    }) {
     const [image, setImage] = useState<string | null>(null);
 
     const showPreviewImage = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (!e.target.files)
             return;
-
+        
         const file = e.target.files[0];
+        setShowImage(true);
+        
         if (file) {
             setImage(URL.createObjectURL(file));
         }
         else {
             setImage(null);
         }
+        
     }
 
     return (
@@ -26,7 +35,7 @@ export default function PreviewImage() {
                 <input type='file' name='product-pic' id='product-pic' onChange={showPreviewImage} />
             </div>
             
-            { image && <Image src={image} className='preview-image' alt={''} width={150} height={150}/>}
+            { (showImage && image) && <Image src={image} className='preview-image' alt={''} width={150} height={150}/>}
         </> 
     )
         
